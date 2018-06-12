03a05d6b3bb0c1c3a967da13c9bffabbdf64c97c
jiesoul <jiesoul@gmail.com>
2018-06-13 01:58:05 +0800

    test

---
+++ main.py
@@ -0,0 +1,15 @@
+import numpy as np
+from keras.models import Sequential
+from keras.layers.core import Dense, Dropout, Activation
+from keras.layers import Conv2D, MaxPooling2D, Flatten
+from keras.optimizers import SGD, Adam
+from keras.utils import np_utils
+from keras.datasets import mnist
+
+def load_data():
+    (x_train, y_train), (x_test, y_test) = mnist.load_data()
+    number = 10000
+    x_train = x_train[0:number]
+    y_train = y_train[0:number]
+
+
